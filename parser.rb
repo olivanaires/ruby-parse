716938b9ae7d17d11ff31ba9a684f37801f40e47
Olivan Aires <olivanaires@gmail.com>
2015-08-20 00:02:10 -0300

    pegando o numero de mortes

--- parser.rb
+++ parser.rb
@@ -1,18 +1,24 @@
 $numberGame=1
+$killCount=0
 
 def print_report()
 	puts "game_#{$numberGame}: {"
+	puts "	total_kills: #{$killCount};"
 	puts "}"
 end
 
 def reset_values()
 	$numberGame += 1
+	$killCount = 0
 end
 
 File.foreach( "games.log" ) do |line|
 	if line.include? "InitGame"
 		print_report()
 		reset_values()
+	end
+	if line.include? "Kill"
+		$killCount += 1
 	end
 end
 
