121423fe1f66958e0160bcacc976eb30159d4414
Olivan Aires <olivanaires@gmail.com>
2015-08-20 00:01:08 -0300

    pegando o numero dos games

--- parser.rb
+++ parser.rb
@@ -1,4 +1,18 @@
+$numberGame=1
+
+def print_report()
+	puts "game_#{$numberGame}: {"
+	puts "}"
+end
+
+def reset_values()
+	$numberGame += 1
+end
+
 File.foreach( "games.log" ) do |line|
-	puts line
+	if line.include? "InitGame"
+		print_report()
+		reset_values()
+	end
 end
 
