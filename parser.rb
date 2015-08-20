50595a1169f662e611125cd2bf8ec6c69d50109e
Olivan Aires <olivanaires@gmail.com>
2015-08-20 00:03:20 -0300

    pegando o nome dos jogadores por game

--- parser.rb
+++ parser.rb
@@ -1,15 +1,28 @@
 $numberGame=1
 $killCount=0
+$players=""
 
 def print_report()
 	puts "game_#{$numberGame}: {"
 	puts "	total_kills: #{$killCount};"
+	puts "	players: #{$players};"
 	puts "}"
+end
+
+def concat_nome_players(nome)
+	if not $players.include? nome
+		if $players != ""
+			$players.concat(", ").concat(nome)	
+		else
+			$players.concat(nome)
+		end 
+	end
 end
 
 def reset_values()
 	$numberGame += 1
 	$killCount = 0
+	$players=""
 end
 
 File.foreach( "games.log" ) do |line|
@@ -19,6 +32,10 @@ File.foreach( "games.log" ) do |line|
 	end
 	if line.include? "Kill"
 		$killCount += 1
+	end
+	if line.include? "ClientUserinfoChanged" 
+		nome = line[/n\\(.*?)\\t/][2..-3]
+		concat_nome_players(nome)
 	end
 end
 
